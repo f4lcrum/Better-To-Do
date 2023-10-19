6de9426b77e5cb7e81db3f0bdc410d6375d4fcce
Martin <martin.stalmach@icontio.com>
2023-10-19 22:20:22 +0200

    fix: remove date from event and create template class

--- src/main/java/cz/muni/fi/pv168/Entity/Template.java
+++ src/main/java/cz/muni/fi/pv168/Entity/Template.java
@@ -1,13 +1,10 @@
 package cz.muni.fi.pv168.Entity;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class Event extends Identifiable {
+public class Template {
     private String name;
     private Status status;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
     private String description;
     private int userId;
 }
