7d550c0c997595ed1503578b61f229cef12f8a0e
Martin <martin.stalmach@icontio.com>
2023-11-09 10:50:48 +0100

    feat: add datetime renderer to event table

--- src/main/java/cz/fi/muni/pv168/bosv/better_todo/ui/panels/EventTablePanel.java
+++ src/main/java/cz/fi/muni/pv168/bosv/better_todo/ui/panels/EventTablePanel.java
@@ -3,16 +3,14 @@ package cz.fi.muni.pv168.bosv.better_todo.ui.panels;
 import cz.fi.muni.pv168.bosv.better_todo.entity.Category;
 import cz.fi.muni.pv168.bosv.better_todo.entity.Status;
 import cz.fi.muni.pv168.bosv.better_todo.ui.model.TodoTableModel;
-import cz.fi.muni.pv168.bosv.better_todo.ui.renderer.CategoryColourRenderer;
-import cz.fi.muni.pv168.bosv.better_todo.ui.renderer.CategoryRenderer;
-import cz.fi.muni.pv168.bosv.better_todo.ui.renderer.LocalDateRenderer;
-import cz.fi.muni.pv168.bosv.better_todo.ui.renderer.StatusRenderer;
+import cz.fi.muni.pv168.bosv.better_todo.ui.renderer.*;
 import lombok.Getter;
 import lombok.NonNull;
 
 import javax.swing.*;
 import java.awt.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @NonNull
 public class EventTablePanel extends JPanel {
@@ -37,7 +35,7 @@ public class EventTablePanel extends JPanel {
         table.setDefaultRenderer(Status.class, new StatusRenderer());
         table.setDefaultRenderer(LocalDate.class, new LocalDateRenderer());
         table.setDefaultRenderer(Color.class, new CategoryColourRenderer());
-
+        table.setDefaultRenderer(LocalDateTime.class, new LocalDateTimeRenderer());
         table.setAutoCreateRowSorter(true);
 
         return table;
---
+++ src/main/java/cz/fi/muni/pv168/bosv/better_todo/ui/renderer/LocalDateTimeRenderer.java
@@ -0,0 +1,19 @@
+package cz.fi.muni.pv168.bosv.better_todo.ui.renderer;
+
+import lombok.NonNull;
+
+import javax.swing.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeRenderer extends AbstractRenderer<LocalDateTime> {
+    public LocalDateTimeRenderer() {
+        super(LocalDateTime.class);
+    }
+
+    @Override
+    protected void updateLabel(@NonNull JLabel label,
+                               @NonNull LocalDateTime value) {
+        label.setText(String.format("%d. %d. %d %02d:%02d", value.getDayOfMonth(), value.getMonthValue(), value.getYear(), value.getHour(), value.getMinute()));
+    }
+}
