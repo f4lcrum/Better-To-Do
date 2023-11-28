c2c3aa68e158a35a33b495db210d355dfb24ce6f
Martin <martin.stalmach@icontio.com>
2023-11-28 19:00:30 +0100

    feat: create transaction connection supplier

---
+++ src/main/java/cz/fi/muni/pv168/todo/storage/sql/db/TransactionConnectionSupplier.java
@@ -0,0 +1,26 @@
+package cz.fi.muni.pv168.todo.storage.sql.db;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * @author Vojtěch Sassmann
+ */
+public class TransactionConnectionSupplier implements Supplier<ConnectionHandler> {
+    private final TransactionManager transactionManager;
+    private final DatabaseManager databaseManager;
+
+    public TransactionConnectionSupplier(TransactionManager transactionManager, DatabaseManager databaseManager) {
+        this.transactionManager = Objects.requireNonNull(transactionManager);
+        this.databaseManager = Objects.requireNonNull(databaseManager);
+    }
+
+    @Override
+    public ConnectionHandler get() {
+        if (transactionManager.hasActiveTransaction()) {
+            return transactionManager.getConnectionHandler();
+        }
+
+        return databaseManager.getConnectionHandler();
+    }
+}
