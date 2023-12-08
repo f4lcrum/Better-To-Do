1b9bee892b802f9d1e7a9b15d5b779f0a9b90edb
Martin <martin.stalmach@icontio.com>
2023-12-08 18:30:13 +0100

    feat: add TimeUnitValidator

---
+++ src/main/java/cz/fi/muni/pv168/todo/business/service/validation/common/TimeUnitValidator.java
@@ -0,0 +1,22 @@
+package cz.fi.muni.pv168.todo.business.service.validation.common;
+
+import cz.fi.muni.pv168.todo.business.entity.TimeUnit;
+import cz.fi.muni.pv168.todo.business.service.validation.ValidationResult;
+import cz.fi.muni.pv168.todo.business.service.validation.Validator;
+
+import java.util.List;
+
+/**
+ * @author Vojtech Sassmann
+ */
+public class TimeUnitValidator implements Validator<TimeUnit> {
+
+    @Override
+    public ValidationResult validate(TimeUnit model) {
+        var validators = List.of(
+                Validator.extracting(TimeUnit::getName, new StringLengthValidator(2, 128, "Time unit name"))
+        );
+
+        return Validator.compose(validators).validate(model);
+    }
+}
