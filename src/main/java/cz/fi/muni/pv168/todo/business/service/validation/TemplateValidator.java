bc8dc922349fd05a655946e63183cee3a6d5ee13
Anna Vítová <xvitova@fi.muni.cz>
2023-12-09 10:16:02 +0100

    fix: Missing space

--- src/main/java/cz/fi/muni/pv168/todo/business/service/validation/TemplateValidator.java
+++ src/main/java/cz/fi/muni/pv168/todo/business/service/validation/TemplateValidator.java
@@ -6,11 +6,12 @@ import cz.fi.muni.pv168.todo.business.service.validation.common.StringLengthVali
 import java.util.List;
 
 /**
- * Department entity {@link Validator}
+ * Template entity {@link Validator}
  */
 public class TemplateValidator implements Validator<Template> {
     @Override
     public ValidationResult validate(Template template) {
+
         var validators = List.of(
                 Validator.extracting(Template::getName, new StringLengthValidator(2, 150, "Template name")),
                 Validator.extracting(Template::getName, new StringLengthValidator(2, 400, "Template description"))
