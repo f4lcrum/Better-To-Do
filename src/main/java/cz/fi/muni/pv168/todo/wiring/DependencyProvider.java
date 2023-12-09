ad95445e37444530c12f0822484d9e316024c682
Anna Vítová <xvitova@fi.muni.cz>
2023-12-09 11:33:56 +0100

    feat(TODO-99): Update Dependency provider

--- src/main/java/cz/fi/muni/pv168/todo/wiring/DependencyProvider.java
+++ src/main/java/cz/fi/muni/pv168/todo/wiring/DependencyProvider.java
@@ -1,6 +1,11 @@
 package cz.fi.muni.pv168.todo.wiring;
 
 import cz.fi.muni.pv168.todo.business.entity.Category;
+import cz.fi.muni.pv168.todo.business.entity.Event;
+import cz.fi.muni.pv168.todo.business.entity.Template;
+import cz.fi.muni.pv168.todo.business.entity.TimeUnit;
+import cz.fi.muni.pv168.todo.business.repository.Repository;
+import cz.fi.muni.pv168.todo.business.service.crud.CrudService;
 import cz.fi.muni.pv168.todo.business.service.validation.Validator;
 import cz.fi.muni.pv168.todo.storage.sql.db.DatabaseManager;
 import cz.fi.muni.pv168.todo.storage.sql.db.TransactionExecutor;
@@ -8,7 +13,19 @@ import cz.fi.muni.pv168.todo.storage.sql.db.TransactionExecutor;
 public interface DependencyProvider {
     DatabaseManager getDatabaseManager();
 
+    Repository<Event> getEventRepository();
+    Repository<Category> getCategoryRepository();
+    Repository<Template> getTemplateRepository();
+    Repository<TimeUnit> getTimeUnitRepository();
+    CrudService<Event> getEventCrudService();
+    CrudService<Category> getCategoryCrudService();
+    CrudService<Template> getTemplateCrudService();
+    CrudService<TimeUnit> getTimeUnitCrudService();
+    Validator<Event> getEventValidator();
     Validator<Category> getCategoryValidator();
+    Validator<Template> getTemplateValidator();
+    Validator<TimeUnit> getTimeUnitValidator();
+
     TransactionExecutor getTransactionExecutor();
 
 }
