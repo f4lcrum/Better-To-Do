31a7ee37dfc47cd896e74f48cd0442c5429fc14c
Martin <martin.stalmach@icontio.com>
2023-12-17 16:24:32 +0100

    feat: simplify (and make better) placeholder; change entity dialog based on tech leads idea

--- src/main/java/cz/fi/muni/pv168/todo/ui/dialog/CategoryDialog.java
+++ src/main/java/cz/fi/muni/pv168/todo/ui/dialog/CategoryDialog.java
@@ -2,14 +2,14 @@ package cz.fi.muni.pv168.todo.ui.dialog;
 
 
 import cz.fi.muni.pv168.todo.business.entity.Category;
+import cz.fi.muni.pv168.todo.ui.custom.PlaceholderTextField;
 
 import javax.swing.JColorChooser;
-import javax.swing.JTextField;
 import java.awt.Color;
 
 public final class CategoryDialog extends EntityDialog<Category> {
 
-    private final JTextField nameField = new JTextField();
+    private final PlaceholderTextField nameField = new PlaceholderTextField();
     private final JColorChooser color = new JColorChooser(Color.BLACK);
 
     private final Category category;
@@ -20,11 +20,6 @@ public final class CategoryDialog extends EntityDialog<Category> {
         if (edit) {
             setValues();
         }
-        setHints();
-    }
-
-    private void setHints() {
-        new TextPrompt("School", nameField);
     }
 
     private void setValues() {
@@ -32,8 +27,8 @@ public final class CategoryDialog extends EntityDialog<Category> {
     }
 
     private void addFields() {
-        add("Name of category: ", nameField, true);
-        add("Color: ", color, true);
+        add("Name of category: ", "School", nameField);
+        addMandatory("Color: ", color);
     }
 
     @Override
--- src/main/java/cz/fi/muni/pv168/todo/ui/dialog/EntityDialog.java
+++ src/main/java/cz/fi/muni/pv168/todo/ui/dialog/EntityDialog.java
@@ -3,12 +3,17 @@ package cz.fi.muni.pv168.todo.ui.dialog;
 import static javax.swing.JOptionPane.OK_CANCEL_OPTION;
 import static javax.swing.JOptionPane.OK_OPTION;
 import static javax.swing.JOptionPane.PLAIN_MESSAGE;
+
+import cz.fi.muni.pv168.todo.ui.custom.PlaceholderLabel;
+import cz.fi.muni.pv168.todo.ui.custom.PlaceholderTextArea;
+import cz.fi.muni.pv168.todo.ui.custom.PlaceholderTextField;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import java.util.Optional;
 
 abstract class EntityDialog<E> {
@@ -19,31 +24,44 @@ abstract class EntityDialog<E> {
         panel.setLayout(new MigLayout("wrap 2"));
     }
 
-    void add(String labelText, JComponent component, boolean isMandatory) {
+    void add(String labelText, String placeholder, PlaceholderTextField component) {
+        component.setPlaceholder(new PlaceholderLabel(placeholder));
+        addMandatory(labelText, component);
+    }
 
-        var label = new JLabel(labelText);
-        if (isMandatory) {
-            label.setText(String.format("<html>%s<font color='red'>%s</font>: </html>", label.getText(), "*"));
-        }
+    void addMandatory(String labelText, JComponent component) {
+        var label = new JLabel(String.format("<html>%s<font color='red'>%s</font>: </html>", labelText, "*"));
+        add(label, component);
+    }
+
+    void addOptional(String labelText, JComponent component) {
+        var label = new JLabel(String.format("%s: ", labelText));
+        add(label, component);
+    }
+
+    private void add(JLabel label, JComponent component) {
         panel.add(label);
         panel.add(component, "wmin 250lp, grow");
     }
 
-    void addDescription(String labelText, JComponent component) {
+    void addDescription(String labelText, String placeholder, PlaceholderTextArea component) {
         var label = new JLabel(String.format("%s: ", labelText));
+        component.setLineWrap(true);
+        component.setPlaceholder(new PlaceholderLabel(placeholder));
+        JScrollPane pane = new JScrollPane(component);
         panel.add(label);
-        panel.add(component, "wmin 250lp, hmin 100lp, grow");
+        panel.add(pane, "wmin 250lp, hmin 100lp, grow");
     }
 
-    void addTime(String labelText, JComponent component1, JComponent component2) {
-        var label = new JLabel(labelText);
-        var hours = new JLabel("h");
-        var minutes = new JLabel("m");
+    void addTime(String labelText, PlaceholderTextField hours, PlaceholderTextField minutes) {
+        var label = new JLabel(String.format("<html>%s<font color='red'>%s</font>: </html>", labelText, "*"));
+        hours.setPlaceholder(new PlaceholderLabel("8"));
+        minutes.setPlaceholder(new PlaceholderLabel("30"));
         panel.add(label);
-        panel.add(component1, "wmin 30, split 4");
-        panel.add(hours);
-        panel.add(component2, "wmin 30");
-        panel.add(minutes);
+        panel.add(hours, "wmin 30, split 4");
+        panel.add(new JLabel("h"));
+        panel.add(minutes, "wmin 30");
+        panel.add(new JLabel("m"));
     }
 
     abstract E getEntity();
