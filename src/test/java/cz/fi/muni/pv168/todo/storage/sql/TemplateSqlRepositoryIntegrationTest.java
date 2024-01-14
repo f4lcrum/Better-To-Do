e11e15644ed3448f4a57f39bca89683c6a0c10b5
Lukas Batora <xlaqo29@gmail.com>
2024-01-14 06:47:43 +0100

    NONE: Revert accidental test deletion

---
+++ src/test/java/cz/fi/muni/pv168/todo/storage/sql/TemplateSqlRepositoryIntegrationTest.java
@@ -0,0 +1,173 @@
+package cz.fi.muni.pv168.todo.storage.sql;
+
+import cz.fi.muni.pv168.todo.business.entity.Category;
+import cz.fi.muni.pv168.todo.business.entity.Template;
+import cz.fi.muni.pv168.todo.business.entity.TimeUnit;
+import cz.fi.muni.pv168.todo.business.repository.CategoryRepository;
+import cz.fi.muni.pv168.todo.business.repository.TemplateRepository;
+import cz.fi.muni.pv168.todo.business.repository.TimeUnitRepository;
+import cz.fi.muni.pv168.todo.storage.sql.dao.DataStorageException;
+import cz.fi.muni.pv168.todo.storage.sql.db.DatabaseManager;
+import cz.fi.muni.pv168.todo.wiring.TestingDependencyProvider;
+import java.awt.Color;
+import java.time.LocalTime;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+final class TemplateSqlRepositoryIntegrationTest {
+    private DatabaseManager databaseManager;
+    private TemplateRepository templateRepository;
+    private CategoryRepository categoryRepository;
+    private TimeUnitRepository timeUnitRepository;
+
+    @BeforeEach
+    void setUp() {
+        databaseManager = DatabaseManager.createTestInstance();
+        var dependencyProvider = new TestingDependencyProvider(databaseManager);
+        templateRepository = dependencyProvider.getTemplateRepository();
+        categoryRepository = dependencyProvider.getCategoryRepository();
+        timeUnitRepository = dependencyProvider.getTimeUnitRepository();
+    }
+
+    @AfterEach
+    void tearDown() {
+        databaseManager.destroySchema();
+    }
+
+    @Test
+    void templateInitSucceeds() {
+        Set<String> expectedTemplatesNames = new HashSet<>(Set.of("Work Template", "Personal Template", "Family Template"));
+        final Collection<Template> retrievedTemplates;
+
+        retrievedTemplates = templateRepository.findAll();
+
+        retrievedTemplates.forEach(timeUnit -> expectedTemplatesNames.remove(timeUnit.getName()));
+
+        // Expecting 4 initial time units - Minute, Hour, Day, Week
+        assertEquals(3, retrievedTemplates.size());
+        assertEquals(0, expectedTemplatesNames.size());
+    }
+
+    @Test
+    void createOneTemplateSucceeds() {
+        final Category newCategory = new Category(UUID.randomUUID(), "TestTemplate", Color.PINK);
+        final TimeUnit newTimeUnit = new TimeUnit(UUID.randomUUID(), false, "TestTU", 10, 120);
+        final Optional<Template> retrievedCategory;
+        final Template newTemplate = new Template(UUID.randomUUID(), "Test Template", "Test Task", newCategory, LocalTime.now(), newTimeUnit, 8, "Template for work tasks");
+
+        categoryRepository.create(newCategory);
+        timeUnitRepository.create(newTimeUnit);
+        templateRepository.create(newTemplate);
+
+        retrievedCategory = assertDoesNotThrow(() -> templateRepository.findByGuid(newTemplate.getGuid()));
+        assertTrue(retrievedCategory.isPresent());
+        assertEquals(newTemplate, retrievedCategory.get());
+    }
+
+    @Test
+    void updateOfInsertedTemplateSucceeds() {
+        final Category newCategory = new Category(UUID.randomUUID(), "TestTemplate", Color.PINK);final TimeUnit newTimeUnit = new TimeUnit(UUID.randomUUID(), false, "TestTU", 10, 120);
+        final Template newTemplate = new Template(UUID.randomUUID(), "Test Template", "Test Task", newCategory, LocalTime.now(), newTimeUnit, 8, "Template for work tasks");
+        final Template updateTemplate = new Template(newTemplate.getGuid(), "Updated Template", "Update Task", newCategory, LocalTime.now(), newTimeUnit, 8, "Upfated template");
+        final Optional<Template> updateResult;
+
+        categoryRepository.create(newCategory);
+        timeUnitRepository.create(newTimeUnit);
+        templateRepository.create(newTemplate);
+
+        assertDoesNotThrow(() -> templateRepository.update(updateTemplate));
+        
+        updateResult = assertDoesNotThrow(() -> templateRepository.findByGuid(newTemplate.getGuid()));
+        assertTrue(updateResult.isPresent());
+        assertEquals(updateTemplate, updateResult.get());
+    }
+
+    @Test
+    void deleteByGuidOnExistingTemplateSucceeds() {
+        final Category newCategory = new Category(UUID.randomUUID(), "TestTemplate", Color.PINK);
+        final TimeUnit newTimeUnit = new TimeUnit(UUID.randomUUID(), false, "TestTU", 10, 120);
+        final Template newTemplate = new Template(UUID.randomUUID(), "Test Template", "Test Task", newCategory, LocalTime.now(), newTimeUnit, 8, "Template for work tasks");
+
+        categoryRepository.create(newCategory);
+        timeUnitRepository.create(newTimeUnit);
+        templateRepository.create(newTemplate);
+
+        assertDoesNotThrow(() -> templateRepository.existsByGuid(newTemplate.getGuid()));
+        assertDoesNotThrow(() -> templateRepository.deleteByGuid(newTemplate.getGuid()));
+        assertThrows(DataStorageException.class, () -> templateRepository.deleteByGuid(newTemplate.getGuid()));
+    }
+
+    @Test
+    void deleteAllSucceeds() {
+        final Category newCategory = new Category(UUID.randomUUID(), "TestTemplate", Color.PINK);
+        final TimeUnit newTimeUnit = new TimeUnit(UUID.randomUUID(), false, "TestTU", 10, 120);
+        final Template newTemplate1 = new Template(UUID.randomUUID(), "Test Template1", "Test Task1", newCategory, LocalTime.now(), newTimeUnit, 8, "Template1 for work tasks");
+        final Template newTemplate2 = new Template(UUID.randomUUID(), "Test Template2", "Test Task2", newCategory, LocalTime.now(), newTimeUnit, 8, "Template2 for work tasks");
+        final Template newTemplate3 = new Template(UUID.randomUUID(), "Test Template3", "Test Task3", newCategory, LocalTime.now(), newTimeUnit, 8, "Template3 for work tasks");
+        final Collection<Template> templates;
+
+        categoryRepository.create(newCategory);
+        timeUnitRepository.create(newTimeUnit);
+        templateRepository.create(newTemplate1);
+        templateRepository.create(newTemplate2);
+        templateRepository.create(newTemplate3);
+
+        templates = templateRepository.findAll();
+        assertDoesNotThrow(() -> templateRepository.deleteAll());
+        assertEquals(3 + 3, templates.size()); // Desired time units were added
+        assertEquals(0, templateRepository.findAll().size()); // All time units were removed
+    }
+
+    @Test
+    void existsByGuidOnExistingTemplateSucceeds() {
+        final Category newCategory = new Category(UUID.randomUUID(), "TestTemplate", Color.PINK);
+        final TimeUnit newTimeUnit = new TimeUnit(UUID.randomUUID(), false, "TestTU", 10, 120);
+        final Template template = new Template(UUID.randomUUID(), "Test Template1", "Test Task1", newCategory, LocalTime.now(), newTimeUnit, 8, "Template1 for work tasks");
+        final boolean existsTemplate;
+
+        categoryRepository.create(newCategory);
+        timeUnitRepository.create(newTimeUnit);
+        templateRepository.create(template);
+
+        existsTemplate = templateRepository.existsByGuid(template.getGuid());
+        assertDoesNotThrow(() -> templateRepository.findByGuid(template.getGuid())); // Time unit actually exists
+        assertTrue(existsTemplate); // existsByGuid returns correct value
+    }
+
+    @Test
+    void existsByGuidOnNonExistingTemplateFails() {
+        final UUID randomUUID = UUID.fromString("d94ee5ef-0325-4ebe-a895-fa380c968f49");
+        final boolean existsResult;
+
+        existsResult = assertDoesNotThrow(() -> templateRepository.existsByGuid(randomUUID));
+        assertFalse(existsResult);
+    }
+
+    @Test
+    void findByGuidOnExistingTemplateSucceeds() {
+        final Category newCategory = new Category(UUID.randomUUID(), "TestTemplate", Color.PINK);
+        final TimeUnit newTimeUnit = new TimeUnit(UUID.randomUUID(), false, "TestTU", 10, 120);
+        final Template template = new Template(UUID.randomUUID(), "Test Template1", "Test Task1", newCategory, LocalTime.now(), newTimeUnit, 8, "Template1 for work tasks");
+        final Optional<Template> retrievedTemplate;
+
+        categoryRepository.create(newCategory);
+        timeUnitRepository.create(newTimeUnit);
+        templateRepository.create(template);
+
+        retrievedTemplate = assertDoesNotThrow(() -> templateRepository.findByGuid(template.getGuid()));
+        assertTrue(retrievedTemplate.isPresent());
+        assertEquals(template, retrievedTemplate.get());
+    }
+}
---
+++ src/test/java/cz/fi/muni/pv168/todo/wiring/TestingDependencyProvider.java
@@ -0,0 +1,88 @@
+package cz.fi.muni.pv168.todo.wiring;
+
+import cz.fi.muni.pv168.todo.business.entity.Template;
+import cz.fi.muni.pv168.todo.business.repository.CategoryRepository;
+import cz.fi.muni.pv168.todo.business.repository.EventRepository;
+import cz.fi.muni.pv168.todo.business.repository.TemplateRepository;
+import cz.fi.muni.pv168.todo.business.repository.TimeUnitRepository;
+import cz.fi.muni.pv168.todo.storage.sql.CategorySqlRepository;
+import cz.fi.muni.pv168.todo.storage.sql.EventSqlRepository;
+import cz.fi.muni.pv168.todo.storage.sql.TemplateSqlRepository;
+import cz.fi.muni.pv168.todo.storage.sql.TimeUnitSqlRepository;
+import cz.fi.muni.pv168.todo.storage.sql.dao.CategoryDao;
+import cz.fi.muni.pv168.todo.storage.sql.dao.EventDao;
+import cz.fi.muni.pv168.todo.storage.sql.dao.TemplateDao;
+import cz.fi.muni.pv168.todo.storage.sql.dao.TimeUnitDao;
+import cz.fi.muni.pv168.todo.storage.sql.db.DatabaseManager;
+import cz.fi.muni.pv168.todo.storage.sql.db.TransactionConnectionSupplier;
+import cz.fi.muni.pv168.todo.storage.sql.db.TransactionManagerImpl;
+import cz.fi.muni.pv168.todo.storage.sql.entity.mapper.CategoryMapper;
+import cz.fi.muni.pv168.todo.storage.sql.entity.mapper.EventMapper;
+import cz.fi.muni.pv168.todo.storage.sql.entity.mapper.TemplateMapper;
+import cz.fi.muni.pv168.todo.storage.sql.entity.mapper.TimeUnitMapper;
+
+public final class TestingDependencyProvider {
+
+    private final TimeUnitRepository timeUnits;
+    private final CategoryRepository categories;
+    private final TemplateRepository templates;
+    private final EventRepository events;
+
+    public TestingDependencyProvider(DatabaseManager databaseManager) {
+        var transactionManager = new TransactionManagerImpl(databaseManager);
+        var transactionConnectionSupplier = new TransactionConnectionSupplier(transactionManager, databaseManager);
+
+        var timeUnitMapper = new TimeUnitMapper();
+        var categoryDao = new CategoryDao(transactionConnectionSupplier);
+        var categoryMapper = new CategoryMapper();
+        var templateMapper = new TemplateMapper(
+                new CategoryDao(transactionConnectionSupplier),
+                categoryMapper,
+                new TimeUnitDao(transactionConnectionSupplier),
+                timeUnitMapper
+        );
+        var eventMapper = new EventMapper(
+                new CategoryDao(transactionConnectionSupplier),
+                categoryMapper,
+                new TimeUnitDao(transactionConnectionSupplier),
+                timeUnitMapper
+        );
+
+        this.timeUnits = new TimeUnitSqlRepository(
+                new TimeUnitDao(transactionConnectionSupplier),
+                timeUnitMapper
+        );
+
+        this.categories = new CategorySqlRepository(
+                categoryDao,
+                categoryMapper
+        );
+
+        this.templates = new TemplateSqlRepository(
+                new TemplateDao(transactionConnectionSupplier),
+                templateMapper
+        );
+
+        this.events = new EventSqlRepository(
+                new EventDao(transactionConnectionSupplier),
+                eventMapper
+        );
+
+    }
+
+    public TimeUnitRepository getTimeUnitRepository() {
+        return timeUnits;
+    }
+    public CategoryRepository getCategoryRepository() {
+        return categories;
+    }
+
+    public TemplateRepository getTemplateRepository() {
+        return templates;
+    }
+
+    public EventRepository getEventRepository() {
+        return events;
+    }
+}
+
