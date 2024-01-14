1d20d1e0efc6835e3d1d5ae65fcc9187f6f018ae
Lukas Batora <xlaqo29@gmail.com>
2024-01-14 09:37:01 +0100

    TODO-142: Implement custom serializer and deserializer for java.awt.Color

--- src/main/java/cz/fi/muni/pv168/todo/io/AbstractImporterExporter.java
+++ src/main/java/cz/fi/muni/pv168/todo/io/AbstractImporterExporter.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import cz.fi.muni.pv168.todo.io.entity.ColorJsonDeserializer;
+import cz.fi.muni.pv168.todo.io.entity.ColorJsonSerializer;
 
 import java.awt.Color;
 
@@ -14,9 +15,10 @@ public abstract class AbstractImporterExporter {
 
     protected AbstractImporterExporter() {
         // Register module to (de)serialize Java.awt.Color
-        final SimpleModule colorDeserializerModuler = new SimpleModule();
-        colorDeserializerModuler.addDeserializer(Color.class, new ColorJsonDeserializer());
-        getObjectMapper().registerModule(colorDeserializerModuler);
+        final SimpleModule colorJsonModule = new SimpleModule();
+        colorJsonModule.addSerializer(Color.class, new ColorJsonSerializer());
+        colorJsonModule.addDeserializer(Color.class, new ColorJsonDeserializer());
+        getObjectMapper().registerModule(colorJsonModule);
         // Register module to be able to interact with Java8 Date&Time API
         objectMapper.registerModule(new JavaTimeModule());
         // Make exported JSON pretty printed
