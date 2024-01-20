483f659315033b5261b3ede9872c091c65b04f2c
Anna Vítová <xvitova@fi.muni.cz>
2024-01-20 17:14:16 +0100

    TODO-147: Add export multiple timeunits test

--- src/test/java/cz/fi/muni/pv168/todo/storage/export/GenericExportServiceIntegrationTest.java
+++ src/test/java/cz/fi/muni/pv168/todo/storage/export/GenericExportServiceIntegrationTest.java
@@ -1,5 +1,10 @@
 package cz.fi.muni.pv168.todo.storage.export;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.fi.muni.pv168.todo.business.entity.Category;
+import cz.fi.muni.pv168.todo.business.entity.Event;
+import cz.fi.muni.pv168.todo.business.entity.Template;
+import cz.fi.muni.pv168.todo.business.entity.TimeUnit;
 import cz.fi.muni.pv168.todo.business.service.crud.CategoryCrudService;
 import cz.fi.muni.pv168.todo.business.service.crud.EventCrudService;
 import cz.fi.muni.pv168.todo.business.service.crud.TemplateCrudService;
@@ -15,6 +20,10 @@ import cz.fi.muni.pv168.todo.storage.memory.InMemoryEventRepository;
 import cz.fi.muni.pv168.todo.storage.memory.InMemoryTemplateRepository;
 import cz.fi.muni.pv168.todo.storage.memory.InMemoryTimeUnitRepository;
 import static org.assertj.core.api.Assertions.assertThat;
+import java.awt.Color;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -30,12 +39,14 @@ class GenericExportServiceIntegrationTest {
 
     private static final Path PROJECT_ROOT = Paths.get(System.getProperty("project.basedir", "")).toAbsolutePath();
     private static final Path TEST_RESOURCES = PROJECT_ROOT.resolve(Path.of("src", "test", "resources"));
+    private static final Path TEST_RESOURCES_EXPECTED = PROJECT_ROOT.resolve(
+            Path.of("src", "test", "resources", "export", "expected")
+    );
 
     private EventCrudService eventCrudService;
     private TemplateCrudService templateCrudService;
     private CategoryCrudService categoryCrudService;
     private TimeUnitCrudService timeUnitCrudService;
-
     private GenericExportService genericExportService;
 
     @BeforeEach
@@ -67,7 +78,7 @@ class GenericExportServiceIntegrationTest {
 
     @Test
     void exportingEmptyDatabaseSucceeds() throws IOException {
-        Path exportResultFilePath = TEST_RESOURCES.resolve("empty-database-export-result.json");
+        Path exportResultFilePath = TEST_RESOURCES.resolve("export/expected/empty-database-export-result.json");
         Path exportFilePath = TEST_RESOURCES.resolve("empty-database-export.json");
         genericExportService.exportData(exportFilePath.toString());
 
@@ -75,6 +86,41 @@ class GenericExportServiceIntegrationTest {
                 .hasSameElementsAs(getFileContents(exportResultFilePath.toString()));
         Files.delete(exportFilePath);
     }
+
+    @Test
+    void exportingMultipleCategories() throws IOException {
+        Category category1 = new Category(UUID.fromString("37a00d72-30ba-4a84-81ba-64393163918d"), "First Category", new Color(0, 0, 0, 255));
+        Category category2 = new Category(UUID.fromString("4d5164d8-5ed4-4694-9165-5119a0ece612"), "Second Category", new Color(255, 175, 175, 255));
+        categoryCrudService.create(category1);
+        categoryCrudService.create(category2);
+
+        Path exportResultFilePath = TEST_RESOURCES_EXPECTED.resolve("multiple-categories-result.json");
+        Path exportFilePath = TEST_RESOURCES.resolve("multiple-categories.json");
+        genericExportService.exportData(exportFilePath.toString());
+
+        assertThat(getFileContents(exportFilePath.toString()))
+                .hasSameElementsAs(getFileContents(exportResultFilePath.toString()));
+        Files.delete(exportFilePath);
+    }
+
+    @Test
+    void exportingMultipleTimeUnits() throws IOException {
+        TimeUnit timeUnit1 = new TimeUnit(UUID.fromString("25aae21c-74f6-425d-8b52-b00cb6b34efb"), true, "Hours", 1, 0);
+        TimeUnit timeUnit2 = new TimeUnit(UUID.fromString("0e587bc6-83a1-44ac-a911-0bfe0df998e4"), true, "Minutes", 2, 1);
+        TimeUnit timeUnit3 = new TimeUnit(UUID.fromString("4253a7c1-0e78-4c24-b97c-810bc15100ae"), false, "Sprint", 3, 54);
+        timeUnitCrudService.create(timeUnit1);
+        timeUnitCrudService.create(timeUnit2);
+        timeUnitCrudService.create(timeUnit3);
+
+        Path exportFilePath = TEST_RESOURCES.resolve("exported-multiple-timeunit.json");
+        Path exportResultFilePath = TEST_RESOURCES_EXPECTED.resolve("multiple-timeunit-result.json");
+        genericExportService.exportData(exportFilePath.toString());
+
+        assertThat(getFileContents(exportFilePath.toString()))
+                .hasSameElementsAs(getFileContents(exportResultFilePath.toString()));
+        Files.delete(exportFilePath);
+    }
+
 
     private List<String> getFileContents(String filePath) throws IOException {
         try (var reader = new BufferedReader(new FileReader(filePath))) {
